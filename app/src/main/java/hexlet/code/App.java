7c6dbd857cf92e0e0179858a2a73331565d512e0
suhoi <sushilyaz2000@gmail.com>
2023-10-25 21:16:50 +0300

    create Differ and add picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,37 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference."
+)
+
+public class App implements Runnable{
+    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
+    private boolean helpRequested = false;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    private boolean versionRequested = false;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+
+    @Override
+    public void run() {
+        if (helpRequested) {
+            CommandLine.usage(this, System.out);
+            return;
+        }
+        if (versionRequested) {
+            System.out.println("Version 1.0");
+            return;
+        }
     }
 }
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,7 @@
+package hexlet.code;
+
+public class Differ {
+    public static void generate() {
+
+    }
+}
